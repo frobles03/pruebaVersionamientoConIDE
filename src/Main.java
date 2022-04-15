8ad7d3b76c4d177d7d058dd906408426de34f722
fer <f.robles03@ufromail.cl>
2022-04-14 21:47:50 -0400

    primer intento del 2022 sin recordar nada

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("prueba de versionamiento con IDE 2.0");
+        System.out.println("la primera no funciono :( \n espero que esta si");
+    }
+}
